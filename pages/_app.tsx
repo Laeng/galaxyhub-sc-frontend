907232c6eb02bd31d7fd91c9e6bc8a0a604723eb
Laeng <Laeng@users.noreply.github.com>
2023-01-11 17:26:44 +0900

    chore: react query, recoil 종속성 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {QueryClient, QueryClientProvider} from "react-query";
+import React from "react";
+import {RecoilRoot} from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </QueryClientProvider>
+  );
 }
