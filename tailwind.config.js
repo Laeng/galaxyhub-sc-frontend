0d8f35032a14b0de69d38693ee1d6e0bfa3d582b
Laeng <Laeng@users.noreply.github.com>
2023-01-11 14:46:01 +0900

    chore: 색상 및 색깔 버튼 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
       fontFamily: {
         sans: ['Pretendard', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        '041bed': '#041b2d',
+        '004e9a': '#004e9a',
+        '428cd4': '#428cd4',
+        'ff9cda': '#ff9cda',
+        'ea4492': '#ea4492'
+      }
     },
   },
   plugins: [],
