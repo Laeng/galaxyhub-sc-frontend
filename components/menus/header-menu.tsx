20778d7974a4a824839d0bab6c0d17e5543674b4
Laeng <Laeng@users.noreply.github.com>
2023-01-11 16:33:00 +0900

    feat: 상단 메뉴 다크 모드 추가 및 메뉴 목록 개선

--- components/menus/header-menu.tsx
+++ components/menus/header-menu.tsx
@@ -1,8 +1,10 @@
-import GalaxyhubLogo from "../brands/galaxyhub-logo";
 import classNames from "classnames";
-import MiddleMenu from "./middle-menu";
 import {useRef, useState} from "react";
-import elementObserver from "../utiles/elementObserver";
+
+import GalaxyhubLogo from "../brands/galaxyhub-logo";
+import ElementObserver from "../utiles/elementObserver";
+import Link from "next/link";
+import hash from "../utiles/hash";
 
 export interface props {
 
@@ -11,16 +13,59 @@ export interface props {
 export default function HeaderMenu({}: props) {
     const [isVisibleTitleArea, setVisibleTitleArea] = useState(true);
     const [isVisibleNavigation, setVisibleNavigation] = useState(false);
-    const titleArea = useRef<HTMLDivElement>(null);
+    const headerMenuTitle = useRef<HTMLDivElement>(null);
 
-    elementObserver(titleArea, setVisibleTitleArea);
+    ElementObserver.isIntersecting(headerMenuTitle, setVisibleTitleArea);
+
+    let [menus] = useState([
+        {
+            title: '저작권 안내',
+            href: '',
+            className: 'md:hover:text-blue-700 dark:md:hover:text-blue-500'
+        },
+        {
+            title: '자주하는 질문',
+            href: '',
+            className: 'md:hover:text-blue-700 dark:md:hover:text-blue-500'
+        },
+        {
+            title: '릴리즈 노트',
+            href: '',
+            className: 'md:hover:text-blue-700 dark:md:hover:text-blue-500'
+        },
+        {
+            title: '디스코드',
+            href: '',
+            className: 'md:hover:text-blue-700 dark:md:hover:text-blue-500'
+        },
+        {
+            title: '갤러리',
+            href: '',
+            className: 'md:hover:text-blue-700 dark:md:hover:text-blue-500'
+        },
+        {
+            title: '연락처',
+            href: '',
+            className: 'md:hover:text-blue-700 dark:md:hover:text-blue-500'
+        },
+        {
+            title: 'LIVE',
+            href: '',
+            className: 'md:float-left md:flex md:items-center md:justify-center md:btn md:btn-rose md:rounded-full md:h-9'
+        },
+        {
+            title: '다운로드',
+            href: '',
+            className: 'md:float-left md:flex md:items-center md:justify-center md:btn md:btn-blue md:rounded-full md:h-9'
+        }
+    ]);
 
     return (
         <div className="absolute flex justify-center w-full z-10">
             <div className={classNames(
-                'fixed max-w-6xl mx-auto py-4 px-4 w-full bg-white z-20',
+                'fixed max-w-6xl mx-auto py-4 px-4 w-full bg-041bed border-b border-gray-800 z-20',
                 'sm:px-6',
-                'md:px-8 md:relative md:bg-transparent md:z-0'
+                'md:px-8 md:relative md:bg-transparent md:dark:bg-transparent md:border-0 md:z-0 '
             )}>
                 <div className={classNames(
                     'flex justify-between items-center w-full',
@@ -28,10 +73,10 @@ export default function HeaderMenu({}: props) {
                 )}>
                     <div className={classNames(
                         'md:flex md:items-end md:justify-between md:pt-4'
-                    )} ref={titleArea}>
+                    )} ref={headerMenuTitle}>
                         <div className={classNames(
-                            'flex space-x-1 md:space-x-0',
-                            'md:block md:font-black md:text-4xl md:space-y-0 dark:md:text-white'
+                            'flex space-x-1 text-white font-medium text-lg',
+                            'md:block md:space-x-0 md:text-041bed md:font-black md:text-4xl md:space-y-0 dark:md:text-white'
                         )}>
                             <p className={classNames(
                                 'md:leading-none'
@@ -67,7 +112,7 @@ export default function HeaderMenu({}: props) {
                             'md:hidden'
                         )} onClick={() => setVisibleNavigation(!isVisibleNavigation)}>
                             <svg className={classNames(
-                                'block h-7 w-7 fill-current'
+                                'block h-7 w-7 fill-current text-white'
                             )} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                 <path d="M0 64H448v64H0V64zM0 224H448v64H0V224zM448 384v64H0V384H448z"/>
                             </svg>
@@ -94,7 +139,7 @@ export default function HeaderMenu({}: props) {
                             }
                         )}>
                             <div className={classNames(
-                                'rounded-t-2xl border-t border-gray-300 bg-white',
+                                'rounded-t-2xl border-t border-gray-300 dark:border-gray-700 bg-white dark:bg-041bed',
                                 'md:p-0 md:items-end md:rounded-none md:border-0 md:bg-transparent',
                                 {
                                     'md:max-w-6xl md:mx-auto md:px-8': !isVisibleTitleArea
@@ -108,8 +153,8 @@ export default function HeaderMenu({}: props) {
                                         'm-4 md:m-0 ml-auto'
                                     )} onClick={() => setVisibleNavigation(!isVisibleNavigation)}>
                                         <svg className={classNames(
-                                            'block h-7 w-7 transition delay-200 opacity-100 fill-current'
-                                        )}>
+                                            'block h-7 w-7 transition delay-200 opacity-100 fill-current dark:text-white'
+                                        )} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                                             <path d="M294.6 166.6L317.3 144 272 98.7l-22.6 22.6L160 210.7 70.6 121.4 48 98.7 2.7 144l22.6 22.6L114.7 256 25.4 345.4 2.7 368 48 413.3l22.6-22.6L160 301.3l89.4 89.4L272 413.3 317.3 368l-22.6-22.6L205.3 256l89.4-89.4z"/>
                                         </svg>
                                         <p className={classNames(
@@ -124,60 +169,30 @@ export default function HeaderMenu({}: props) {
                                     'md:h-auto md:max-h-fit'
                                 )}>
                                     <ul className={classNames(
-                                        'navigation w-full text-center border-b border-gray-300 font-medium',
+                                        'navigation w-full text-center border-b border-gray-300 dark:border-gray-700 font-medium',
                                         'md:flex md:items-center md:border-0 dark:md:text-white'
                                     )}>
-                                        <li className={classNames(
-                                            'group text-lg py-2 border-t md:border-0 border-gray-300',
-                                            'md:float-left md:text-base md:pr-4'
-                                        )}>
-                                            저작권 안내
-                                        </li>
-                                        <li className={classNames(
-                                            'group text-lg py-2 border-t md:border-0 border-gray-300',
-                                            'md:float-left md:text-base md:pr-4'
-                                        )}>
-                                            자주하는 질문
-                                        </li>
-                                        <li className={classNames(
-                                            'group text-lg py-2 border-t md:border-0 border-gray-300',
-                                            'md:float-left md:text-base md:pr-4'
-                                        )}>
-                                            릴리즈 노트
-                                        </li>
-                                        <li className={classNames(
-                                            'group text-lg py-2 border-t md:border-0 border-gray-300',
-                                            'md:float-left md:text-base md:pr-4'
-                                        )}>
-                                            디스코드
-                                        </li>
-                                        <li className={classNames(
-                                            'group text-lg py-2 border-t md:border-0 border-gray-300',
-                                            'md:float-left md:text-base md:pr-4'
-                                        )}>
-                                            갤러리
-                                        </li>
-                                        <li className={classNames(
-                                            'group text-lg py-2 border-t md:border-0 border-gray-300',
-                                            'md:float-left md:text-base md:pr-4'
-                                        )}>
-                                            연락처
-                                        </li>
-                                        <li className={classNames(
-                                            'group text-lg py-2 border-t md:border-0 border-gray-300',
-                                            'md:float-left md:flex md:items-center md:justify-center md:btn md:btn-rose md:rounded-full h-9 md:ml-auto md:mr-4'
-                                        )}>
-                                            LIVE
-                                        </li>
-                                        <li className={classNames(
-                                            'group text-lg py-2 border-t md:border-0 border-gray-300',
-                                            'md:float-left md:flex md:items-center md:justify-center md:btn md:btn-blue md:rounded-full h-9'
-                                        )}>
-                                            다운로드
-                                        </li>
+                                        {Object.values(menus).map((menu, id) => (
+                                            <li className={classNames(
+                                                'flex group text-lg py-2 border-t md:border-0 border-gray-300 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 dark:text-white',
+                                                'md:block md:float-left md:text-base md:hover:bg-transparent',
+                                                {
+                                                    'md:pr-4': id !== menus.length - 1,
+                                                    'ml-auto': id === menus.length - 2
+                                                }
+                                            )} key={hash(menu.title)}>
+                                                <Link className={classNames(
+                                                    'w-full text-center',
+                                                    menu.className
+                                                )} href={menu.href}>
+                                                    {menu.title}
+                                                </Link>
+                                            </li>
+                                        ))}
+
                                     </ul>
                                     <div className={classNames(
-                                        'flex justify-center group my-2',
+                                        'flex justify-center group my-2 dark:text-gray-500',
                                         'md:hidden'
                                     )}>
                                         <div className={classNames(
